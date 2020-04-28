1dcfda28588443936dd0765194b81e7f98ec2421
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-04-28 11:55:17 -0400

    adds exercise ajax102

--- ajax1.js
+++ ajax1.js
@@ -1,8 +1,31 @@
 const dogButton = document.getElementById("generate_dog");
+let url2 = "https://dog.ceo/api/breeds/list";
 let url = "https://dog.ceo/api/breed/rottweiler/images/random";
+let url3 = `https://dog.ceo/api/${this.value}/rottweiler/images/random`;
 const div1 = document.getElementById("img_contain");
 let img = document.createElement("img");
+let dropDown = document.getElementById("drop");
 
+console.log($.get(url2));
+$.get(url2)
+    .then((data) => {
+        for (let i = 0; i < data.message.length; i++) {
+            let newOption = document.createElement("option");
+            newOption.setAttribute("value", data.message[i]);
+            newOption.textContent = data.message[i];
+            dropDown.appendChild(newOption);
+        }
+    })
+
+dropDown.addEventListener("change", (e) => {
+    let value = e.target.value;
+    $.get(`https://dog.ceo/api/breed/${value}/images/random`)
+
+    .then((data) => {
+        img.setAttribute("src", data.message);
+        div1.appendChild(img);
+    })
+})
 
 dogButton.addEventListener("click", () => {
     dogButton.textContent = "Generating Dog..."
@@ -10,5 +33,6 @@ dogButton.addEventListener("click", () => {
         .then((data) => {
             img.setAttribute("src", data.message);
             div1.appendChild(img);
-        })
+            dogButton.textContent = "Generate Dog";
+        })    
 })
